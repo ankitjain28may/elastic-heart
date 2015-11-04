b45c38f31ee83ba60a1c178e97b4f63801acfa9c
Abhay <abhayrawat2695@gmail.com>
2015-11-05 03:17:02 +0530

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,26 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Laravel</title>
-        <meta name="google-signin-client_id" content="911724778979-eulj868cugcjcikpbeislco41ua7o0sn.apps.googleusercontent.com">
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-        <style>
-            html, body {
-                height: 100%;
-            }
+<head>
+    <title>Welcome to Elastic Heart</title>
+    <meta name="google-signin-client_id" content="911724778979-eulj868cugcjcikpbeislco41ua7o0sn.apps.googleusercontent.com">
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="{{asset("../vendor/twbs/bootstrap/dist/css/bootstrap.css")}}">
+</head>
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
+<style type="text/css">
+    body, html{
+        height: 100%
+    }
+    .container{
+        margin-top: 45%;
+    }
+</style>
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
+<body>
+    <div class = 'container'>
+        <button class="btn btn-default">Sign in using Google</button>
+        <button class="btn btn-default">Sign in using Facebook</button>
+    </div>
+</body>
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
-            <p>Sign in</p>
-        </div>
-    </body>
 </html>

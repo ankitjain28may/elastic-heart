7f19e19c39d05764475c8dcbd23bd7552dc21234
Nelabh Kotiya <nelabhkotiya@gmail.com>
2015-11-07 01:36:52 +0530

    Added Google Login Button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,9 @@
 <html>
 <head>
     <title>Welcome to Elastic Heart</title>
-    <meta name="google-signin-client_id" content="911724778979-eulj868cugcjcikpbeislco41ua7o0sn.apps.googleusercontent.com">
+    <meta name="google-signin-client_id" content="1024750123636-4m9cf0ra6js17fkci5gkpbr9ajeq1k91.apps.googleusercontent.com">
     <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" type="text/css" href="{{asset("../vendor/twbs/bootstrap/dist/css/bootstrap.css")}}">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
 </head>
 
 <style type="text/css">
@@ -18,7 +18,7 @@
 
 <body>
     <div class = 'container'>
-        <button class="btn btn-default">Sign in using Google</button>
+        <a  href="{{URL::asset('')}}"><img src="{{URL::asset('assets/img/login.png')}}"></a>
         <button class="btn btn-default">Sign in using Facebook</button>
     </div>
 </body>

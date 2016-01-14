bc92fd98cd806dbb33768a5a2e31b1b46d271729
sahilkmaurya <sahil.kr.maurya@gmail.com>
2016-01-14 23:28:01 +0530

    Database schema added

--- database/migrations/2016_01_14_161440_create_admin_details.php
+++ database/migrations/2016_01_14_161440_create_admin_details.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateAdminDetails extends Migration
 {
     /**
      * Run the migrations.
@@ -12,14 +12,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('admin_details',function(Blueprint $table){
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->rememberToken();
+            $table->string('email');
+            $table->foreign('email')->references('email')->on('users');
+            $table->remembertoken();
             $table->timestamps();
-        });
+    });
     }
 
     /**
@@ -29,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('admin_details');
     }
 }

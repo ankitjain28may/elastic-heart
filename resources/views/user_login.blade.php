fac6104ef4f9fac87773af3156ea4438b9458347
sahilkmaurya <sahil.kr.maurya@gmail.com>
2016-01-19 21:22:05 +0530

    veiws added

---
+++ resources/views/user_login.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<h1>User Log in</h1>
+<form action="" method="">
+<label for="email">E-mail</label><br>
+<input type="email" name="email" placeholder="E-mail"><br>
+<label for="pwd">Password</label><br>
+<input type="password" name="password" placeholder="Password">
+<br><input type="submit" value="submit">
+</form>
+</body>
+</html>

292e1c99dabd8e94984abf95c0aebb58a0a54562
Sushumna Khandelwal <sush.94kh@gmail.com>
2016-03-22 22:59:36 +0530

    Menu Modularity

--- admin/resources/views/common.blade.php
+++ admin/resources/views/common.blade.php
@@ -24,8 +24,7 @@
     <link href="{{asset('dist/css/sb-admin-2.css')}}" rel="stylesheet">
 
     <!-- Morris Charts CSS -->
-    <link href="{{asset('bower_components/morrisjs/morris.css')}}" rel="stylesheet">
-
+    
     <!-- Custom Fonts -->
     <link href="{{asset('bower_components/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
 
@@ -34,87 +33,66 @@
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+        <![endif]-->
 
-</head>
+    </head>
 
-<body>
+    <body>
 
-    <div id="wrapper">
+        <div id="wrapper">
 
-        <!-- Navigation -->
-        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="{{url('/')}}">Plexus.</a>
-            </div>
-            <!-- /.navbar-header -->
-
-            <ul class="nav navbar-top-links navbar-right">
-            <!-- /.dropdown -->
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                        <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
-                    </a>
-                    <ul class="dropdown-menu dropdown-user">
-                        <li><a href="{{url('logout')}}"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
-                        </li>
-                    </ul>
-                    <!-- /.dropdown-user -->
-                </li>
-                <!-- /.dropdown -->
-            </ul>
-            <!-- /.navbar-top-links -->
-
-            <div class="navbar-default sidebar" role="navigation">
-                <div class="sidebar-nav navbar-collapse">
-                    <ul class="nav" id="side-menu">
-                        <li>
-                            <a href="index.html"><i class="fa fa-dashboard fa-fw"></i>Dashboard</a>
-                        </li>
-                        <li>
-                            <a href="forms.html"><i class="fa fa-dashboard fa-fw"></i>Add Event</a>
-                        </li>
-                         <li>
-                            <a href="tables.html"><i class="fa fa-dashboard fa-fw"></i>View Event</a>
-                        </li>
-                       <li>
-                            <a href="tables.html"><i class="fa fa-table fa-fw"></i>Delete Event</a>
-                        </li>
-                        <li>
-                            <a href="notifications.html"><i class="fa fa-table fa-fw"></i>Notifications</a>
-                        </li>
-                    </ul>
+            <!-- Navigation -->
+            <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="{{url('/')}}">Plexus.</a>
                 </div>
-                <!-- /.sidebar-collapse -->
-            </div>
-            <!-- /.navbar-static-side -->
-        </nav>
+                <!-- /.navbar-header -->
 
-    @yield('content')        
+                <ul class="nav navbar-top-links navbar-right">
+                    <!-- /.dropdown -->
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                            <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
+                        </a>
+                        <ul class="dropdown-menu dropdown-user">
+                            <li><a href="{{url('logout')}}"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                            </li>
+                        </ul>
+                        <!-- /.dropdown-user -->
+                    </li>
+                    <!-- /.dropdown -->
+                </ul>
+                <!-- /.navbar-top-links -->
 
-        <!-- /#page-wrapper -->
+                @include('menu')
+                    <!-- /.navbar-static-side -->
+            </nav>
 
-    </div>
-    <!-- /#wrapper -->
+            @yield('content')        
 
-    <!-- jQuery -->
-    <script src="{{asset('bower_components/jquery/dist/jquery.min.js')}}"></script>
+            <!-- /#page-wrapper -->
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="{{asset('bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+        </div>
+        <!-- /#wrapper -->
 
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="')}}{{asset('bower_components/metisMenu/dist/metisMenu.min.js')}}"></script>
+        <!-- jQuery -->
+        <script src="{{asset('bower_components/jquery/dist/jquery.min.js')}}"></script>
 
-    <!-- Custom Theme JavaScript -->
-    <script src="{{asset('dist/js/sb-admin-2.js')}}"></script>
+        <!-- Bootstrap Core JavaScript -->
+        <script src="{{asset('bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
 
-</body>
+        <!-- Metis Menu Plugin JavaScript -->
+        <script src="{{asset('bower_components/metisMenu/dist/metisMenu.min.js')}}"></script>
 
-</html>
+        <!-- Custom Theme JavaScript -->
+        <script src="{{asset('dist/js/sb-admin-2.js')}}"></script>
+
+    </body>
+
+    </html>
---
+++ admin/resources/views/menu.blade.php
@@ -0,0 +1,28 @@
+<div class="navbar-default sidebar" role="navigation">
+    <div class="sidebar-nav navbar-collapse">
+        <ul class="nav" id="side-menu">
+            <li>
+                <a href="{{route('root')}}"><i class="fa fa-dashboard fa-fw"></i>Dashboard</a>
+            </li>
+            <li>
+                <a href="#"><i class="fa fa-dashboard fa-fw"></i> Events<span class="fa arrow"></span></a>
+                <ul class="nav nav-second-level">
+                    <li>
+                        <a href="{{route('root')}}"> Add Event</a>
+                    </li>
+                    <li>
+                        <a href="{{route('root')}}"> View Events</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li>
+                <a href="tables.html"><i class="fa fa-table fa-fw"></i>Delete Event</a>
+            </li>
+            <li>
+                <a href="notifications.html"><i class="fa fa-table fa-fw"></i>Notifications</a>
+            </li>
+        </ul>
+    </div>
+    <!-- /.sidebar-collapse -->
+</div>

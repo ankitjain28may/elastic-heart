5430ce027e1f0df5dfbd514ef5738e2cfd305ffb
Sushumna Khandelwal <sush.94kh@gmail.com>
2016-04-04 01:13:45 +0530

    .

--- admin/resources/views/view_questions.blade.php
+++ admin/resources/views/view_questions.blade.php
@@ -36,11 +36,16 @@
         <tr>
             <td><?php $i;?></td>
             <td>{!!$d->question!!}</td>
-            <td></td>
+            <?php   $a = unserialize($d->options);?>
+            @foreach($a as $opt)
+            <td>{!!$opt!!}</td>
+            @endforeach
             <td>{!!$d->file!!}</td>
             <td>{!!$d->image!!}</td>
-            <td>{!!$d->html!!}</td>
-            <td>Answer</td>
+            <td>{!!$d->html!!}</td>`
+            @foreach($ans as $a)
+            <td>{!!$a->answer!!}</td>
+            @endforeach
             <td><a class="btn btn-info btn-xs"
                 role="edit_button" href="editquestion/{{$d->id}}">
                 Edit</a>

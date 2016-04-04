1a8534821709819af6632fdca30c38a059d50d76
Nelabh Kotiya <nelabhkotiya@gmail.com>
2016-04-04 13:56:56 +0530

    Migration Foreign Key Updated

--- database/migrations/2016_03_28_204436_create_answers_table.php
+++ database/migrations/2016_03_28_204436_create_answers_table.php
@@ -15,7 +15,7 @@ class CreateAnswersTable extends Migration
         Schema::create('answers', function(Blueprint $table) {
             $table->increments('id');
             $table->integer('ques_id')->length(10)->unsigned();
-            $table->foreign('id')->references('id')->on('questions')->onDelete('cascade'); 
+            $table->foreign('ques_id')->references('id')->on('questions')->onDelete('cascade'); 
             $table->integer('score');
             $table->string('answer', 1000);
             $table->integer('incorrect')->default(0);

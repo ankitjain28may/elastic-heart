a39bb9301d9ed8f50d5d564982c4f439115b76fb
Sushumna Khandelwal <sush.94kh@gmail.com>
2016-04-04 12:29:58 +0530

    Edit Ques

--- admin/app/Http/Controllers/PagesController.php
+++ admin/app/Http/Controllers/PagesController.php
@@ -164,4 +164,71 @@ public function deletequestion($id)
   return Redirect::route('viewquestions',compact('event_id'));
 
 }
+
+public function edit_question()
+{
+  
+
+    $data=Question::where('id','=',$id)->first();
+    $event_id = $data->event_id;
+    $data = Input::all();
+    $question->event_id = Session::get('event_id');
+    $question->question = $data['question'];
+    $image = array();
+    if(isset($data['file'])){
+    if (Input::file('file')->isValid()){
+      $destinationPathvfile = 'uploads';
+      $extensionvfile = Input::file('file')->getClientOriginalExtension(); 
+      $fileNamevfile = $event->id.'.'.$extensionvfile; // renaming image
+      Input::file('file')->move($destinationPathvfile, $fileNamevfile);
+      $question->image = $fileNamevfile;
+    }
+}
+
+
+
+    if(isset($data['html'])){
+      $question->html = $data['html'];      
+    }
+    if(intval($event->type) > 2){
+     $question->options = serialize($data['options']);
+     $answers = $data['answers'];
+  $question->save();
+  Session::put('qid',Question::all()->last()->id); 
+
+     foreach($answers as $ans){
+   
+      $answer->ques_id = Session::get('qid');
+      $answer->answer = $ans;
+      $answer->score = 1;
+      $answer->incorrect = 0;
+      $answer->save();
+    }
+  }
+  else{
+    $question->level = $data['level'];
+    $question->save();
+  Session::put('qid',Question::all()->last()->id);   
+    $answer->ques_id = Session::get('qid');
+    $answer->answer = $data['answer'];
+    $answer->score = 1;
+    $answer->incorrect = 0;
+    $answer->save();
+  }
+    Session::put('event_id',$event->id);
+
+  return Redirect::route('edit_questions',compact('event_id'));
+}
+
+public function editquestion($id)
+{
+ $data=Question::where('id','=',$id)->first();
+    Session::put('qid',$id);
+    $type = Event::where('id',$data->event_id)->first()->type;
+      $data->ans = Answer::where('ques_id',$id)->get()->pluck('answer')->toArray();
+  return \View::make('edit_ques',['data'=>$data,'type'=>$type]);
+}
+
+  //$data=Question::where('event_id','=',Session::get('event_id'));
+  
 }
